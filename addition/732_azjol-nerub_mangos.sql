4f2e999e147e14370dc73d92c5ec69645d53d790
przemratajczak <przemratajczak@wp.pl>
2011-04-16 02:09:11 +0200

    restored orginal SD2 script for KrikThir (AzjolNerub) - Looks and works better

--- addition/732_azjol-nerub_mangos.sql
+++ addition/732_azjol-nerub_mangos.sql
@@ -1,10 +1,6 @@
 -- Azjol-Nerub from MaxXx2021
-UPDATE creature_template SET ScriptName = 'npc_watcher_gashra' WHERE entry = 28730;
-UPDATE creature_template SET ScriptName = 'npc_watcher_narjil' WHERE entry = 28729;
-UPDATE creature_template SET ScriptName = 'boss_krikthir' WHERE entry = 28684;
-UPDATE creature_template SET ScriptName = 'npc_watcher_silthik' WHERE entry = 28731;
 UPDATE creature_template SET ScriptName = 'boss_hadronox' WHERE entry = 28921;
-UPDATE creature_template SET ScriptName = 'npc_anub_ar_warrior' WHERE entry = 28732;
-UPDATE creature_template SET ScriptName = 'npc_anub_ar_skirmisher' WHERE entry = 28734;
-UPDATE creature_template SET ScriptName = 'npc_anub_ar_shadowcaster' WHERE entry = 28733;
-UPDATE creature_template SET ScriptName = 'npc_skittering_infector' WHERE entry = 28736;
+
+-- Anub'ar Warrior
+INSERT INTO `creature_ai_Scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
+('2873203','28732','0','0','100','7','1000','2000','18000','27000','13','-100','1','0','11','52538','3','0','0','0','0','0','Anub\'ar Warrior - Cast Charge');
