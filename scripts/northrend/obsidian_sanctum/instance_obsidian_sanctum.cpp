ddf5f06c403e8feed1d64278ed53f075af83d3fa
Grandres <grundil@wp.pl>
2011-05-03 14:42:52 +0200

    [s27] Instance Obisidian Sanctum backported old version

--- scripts/northrend/obsidian_sanctum/instance_obsidian_sanctum.cpp
+++ scripts/northrend/obsidian_sanctum/instance_obsidian_sanctum.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2011 ScriptDev2 <http://www.scriptdev2.com/>
+/* Copyright (C) 2006 - 2010 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,7 +16,7 @@
 
 /* ScriptData
 SDName: Instance_Obsidian_Sanctum
-SD%Complete: 80%
+SD%Complete: 90%
 SDComment:
 SDCategory: Obsidian Sanctum
 EndScriptData */
@@ -28,19 +28,29 @@ EndScriptData */
 0 - Sartharion
 */
 
-instance_obsidian_sanctum::instance_obsidian_sanctum(Map* pMap) : ScriptedInstance(pMap),
-    m_uiAliveDragons(0),
-    m_uiSartharionGUID(0),
-    m_uiTenebronGUID(0),
-    m_uiShadronGUID(0),
-    m_uiVesperonGUID(0)
-    {
-        Initialize();
-    }
+instance_obsidian_sanctum::instance_obsidian_sanctum(Map* pMap) : ScriptedInstance(pMap)
+{
+    Initialize();
+};
 
 void instance_obsidian_sanctum::Initialize()
 {
     memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+
+    m_uiSartharionGUID = 0;
+    m_uiTenebronGUID   = 0;
+    m_uiShadronGUID    = 0;
+    m_uiVesperonGUID   = 0;
+
+    m_uiAcolyteShadronGUID  = 0;
+    m_uiAcolyteVesperonGUID = 0;
+
+    m_lTrashMobsGUIDlist.clear();
+    m_lTrashMobsGUIDlist.clear();
+    m_lEggsGUIDList.clear();
+    m_lWhelpsGUIDList.clear();
+    m_lBlazesGUIDList.clear();
+    //m_lHitByVolcanoGUIDList.clear();
 }
 
 void instance_obsidian_sanctum::OnCreatureCreate(Creature* pCreature)
@@ -50,8 +60,8 @@ void instance_obsidian_sanctum::OnCreatureCreate(Creature* pCreature)
         case NPC_SARTHARION:
             m_uiSartharionGUID = pCreature->GetGUID();
             break;
-        // The three dragons below set to active state once created.
-        // We must expect bigger raid to encounter main boss, and then three dragons must be active due to grid differences
+        //three dragons below set to active state once created.
+        //we must expect bigger raid to encounter main boss, and then three dragons must be active due to grid differences
         case NPC_TENEBRON:
             m_uiTenebronGUID = pCreature->GetGUID();
             pCreature->SetActiveObjectState(true);
@@ -64,56 +74,111 @@ void instance_obsidian_sanctum::OnCreatureCreate(Creature* pCreature)
             m_uiVesperonGUID = pCreature->GetGUID();
             pCreature->SetActiveObjectState(true);
             break;
+        // trash mobs aggro when Sartharion is engaged
+        case NPC_ONYX_BROOD_GENERAL:
+        case NPC_ONYX_BLAZE_MISTRESS:
+        case NPC_ONYX_FLIGHT_CAPTAIN:
+        case NPC_ONYX_SANCTUM_GUARDIAN:
+            m_lTrashMobsGUIDlist.push_back(pCreature->GetGUID());
+            break;
     }
 }
 
 void instance_obsidian_sanctum::SetData(uint32 uiType, uint32 uiData)
 {
-    if (uiType == TYPE_SARTHARION_EVENT)
-        m_auiEncounter[0] = uiData;
-    else if (uiType == TYPE_ALIVE_DRAGONS)
-        m_uiAliveDragons = uiData;
-
-    // No need to save anything here
+    m_auiEncounter[uiType - 1] = uiData;
 }
 
 uint32 instance_obsidian_sanctum::GetData(uint32 uiType)
 {
-    if (uiType == TYPE_SARTHARION_EVENT)
-        return m_auiEncounter[0];
-
-    return 0;
+    return m_auiEncounter[uiType - 1];
 }
 
 uint64 instance_obsidian_sanctum::GetData64(uint32 uiData)
 {
     switch(uiData)
     {
-        case NPC_SARTHARION: return m_uiSartharionGUID;
-        case NPC_TENEBRON:   return m_uiTenebronGUID;
-        case NPC_SHADRON:    return m_uiShadronGUID;
-        case NPC_VESPERON:   return m_uiVesperonGUID;
-        default:
-            return 0;
+        case DATA_SARTHARION:
+            return m_uiSartharionGUID;
+        case DATA_TENEBRON:
+            return m_uiTenebronGUID;
+        case DATA_SHADRON:
+            return m_uiShadronGUID;
+        case DATA_VESPERON:
+            return m_uiVesperonGUID;
+        case DATA_ACOL_SHAD:
+            return m_uiAcolyteShadronGUID;
+        case DATA_ACOL_VESP:
+            return m_uiAcolyteVesperonGUID;
     }
+    return 0;
 }
 
-bool instance_obsidian_sanctum::CheckAchievementCriteriaMeet(uint32 uiCriteriaId, Player const* pSource, Unit const* pTarget, uint32 uiMiscValue1 /* = 0*/)
+bool instance_obsidian_sanctum::IsEncounterInProgress() const
+{
+    for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+        if (m_auiEncounter[i] == IN_PROGRESS)
+            return true;
+
+    return false;
+}
+
+bool instance_obsidian_sanctum::CheckConditionCriteriaMeet(Player const* pSource, uint32 uiMapId, uint32 uiInstanceConditionId)
+{
+    return (GetData(uiInstanceConditionId) == DONE);
+}
+
+bool instance_obsidian_sanctum::CheckAchievementCriteriaMeet(uint32 uiCriteriaId, Player const* pSource, Unit const* pTarget, uint32 uiMiscValue1)
 {
     switch (uiCriteriaId)
     {
-        case ACHIEV_DRAGONS_ALIVE_1_N:
-        case ACHIEV_DRAGONS_ALIVE_1_H:
-            return m_uiAliveDragons >= 1;
-        case ACHIEV_DRAGONS_ALIVE_2_N:
-        case ACHIEV_DRAGONS_ALIVE_2_H:
-            return m_uiAliveDragons >= 2;
-        case ACHIEV_DRAGONS_ALIVE_3_N:
-        case ACHIEV_DRAGONS_ALIVE_3_H:
-            return m_uiAliveDragons >= 3;
-        default:
-            return false;
+        case ACHIEV_CRIT_ASSIST_10:
+            if (instance->IsRegularDifficulty())
+                return GetData(TYPE_SARTH_HARD_ONE) == DONE;
+            break;
+        case ACHIEV_CRIT_ASSIST_25:
+            if (!instance->IsRegularDifficulty())
+                return GetData(TYPE_SARTH_HARD_ONE) == DONE;
+            break;
+        case ACHIEV_CRIT_DUO_10:
+            if (instance->IsRegularDifficulty())
+                return GetData(TYPE_SARTH_HARD_TWO) == DONE;
+            break;
+        case ACHIEV_CRIT_DUO_25:
+            if (!instance->IsRegularDifficulty())
+                return GetData(TYPE_SARTH_HARD_TWO) == DONE;
+            break;
+        case ACHIEV_CRIT_ZONE_10:
+            if (instance->IsRegularDifficulty())
+                return GetData(TYPE_SARTH_HARD_THREE) == DONE;
+            break;
+        case ACHIEV_CRIT_ZONE_25:
+            if (!instance->IsRegularDifficulty())
+                return GetData(TYPE_SARTH_HARD_THREE) == DONE;
+            break;
+        case ACHIEV_CRIT_VOLCANO_10:
+            if (instance->IsRegularDifficulty())
+            {
+                for (std::list<uint64>::iterator i = m_lHitByVolcanoGUIDList.begin(); i != m_lHitByVolcanoGUIDList.end(); i++)
+                    if (pSource->GetGUID() == *i)
+                        return false;
+
+                return true;
+            }
+            break;
+        case ACHIEV_CRIT_VOLCANO_25:
+            if (!instance->IsRegularDifficulty())
+            {
+                for (std::list<uint64>::iterator i = m_lHitByVolcanoGUIDList.begin(); i != m_lHitByVolcanoGUIDList.end(); i++)
+                    if (pSource->GetGUID() == *i)
+                        return false;
+
+                return true;
+            }
+            break;
     }
+
+    return false;
 }
 
 InstanceData* GetInstanceData_instance_obsidian_sanctum(Map* pMap)
@@ -123,10 +188,9 @@ InstanceData* GetInstanceData_instance_obsidian_sanctum(Map* pMap)
 
 void AddSC_instance_obsidian_sanctum()
 {
-    Script* pNewScript;
-
-    pNewScript = new Script;
-    pNewScript->Name = "instance_obsidian_sanctum";
-    pNewScript->GetInstanceData = GetInstanceData_instance_obsidian_sanctum;
-    pNewScript->RegisterSelf();
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name = "instance_obsidian_sanctum";
+    newscript->GetInstanceData = GetInstanceData_instance_obsidian_sanctum;
+    newscript->RegisterSelf();
 }
