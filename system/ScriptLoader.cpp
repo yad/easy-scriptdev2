5245ea092592a1df5f6dbd41b1a506dade444561
Grandres <grundil@wp.pl>
2011-05-13 18:28:07 +0200

    Removed unnessecary lines from ScriptLoader.cpp

--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -391,7 +391,6 @@ extern void AddSC_boss_xt002();
 extern void AddSC_boss_yogg_saron();
 extern void AddSC_instance_ulduar();
 extern void AddSC_ulduar();
-extern void AddSC_ulduar_teleport();
 extern void AddSC_boss_ingvar();                            //utgarde_keep
 extern void AddSC_boss_keleseth();
 extern void AddSC_boss_skarvald_and_dalronn();
@@ -914,7 +913,6 @@ void AddScripts()
     AddSC_boss_yogg_saron();
     AddSC_instance_ulduar();
     AddSC_ulduar();
-    AddSC_ulduar_teleport();
     AddSC_boss_ingvar();                                    //utgarde_keep
     AddSC_boss_keleseth();
     AddSC_boss_skarvald_and_dalronn();
