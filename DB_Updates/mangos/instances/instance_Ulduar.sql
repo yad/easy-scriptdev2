5e1d1791c5b745d34824b95b2254a1fd0712f083
michalpolko <michalpolko@o2.pl>
2011-05-24 00:39:35 +0200

    [s69] Some minor fixes for Mimiron script.

--- DB_Updates/mangos/instances/instance_Ulduar.sql
+++ DB_Updates/mangos/instances/instance_Ulduar.sql
@@ -731,3 +731,75 @@ INSERT INTO gameobject_loot_template VALUES
 DELETE FROM reference_loot_template WHERE item = 45038;
 INSERT INTO reference_loot_template VALUES
 (33886, 45038, 14, 3, 1, 1, 18, 603, 38); -- XT-002 hard %
+
+-- Mimiron
+UPDATE `gameobject_template` SET `data0` = '60000' WHERE `entry` =194675;
+UPDATE creature_template SET ScriptName = 'boss_mimiron' WHERE entry = 33350;
+UPDATE creature_template SET `RegenHealth`= 0, ScriptName = 'boss_leviathan_mk' WHERE entry = 33432;
+UPDATE creature_template SET ScriptName = 'leviathan_turret' WHERE entry = 34071;
+UPDATE creature_template SET ScriptName = 'mob_mimiron_flames' WHERE entry IN (34363, 34121);
+UPDATE creature_template SET `RegenHealth`= 0, ScriptName = 'boss_vx001' WHERE entry = 33651;
+UPDATE creature_template SET `RegenHealth`= 0, ScriptName = 'boss_aerial_command_unit' WHERE entry = 33670;
+UPDATE creature SET position_x = 2784.35, position_y = 2578.03, orientation = 3.2 WHERE id = 33350;
+UPDATE creature SET position_x = 2794.86, position_y = 2597.83, orientation = 3.57, spawnMask = 3 WHERE id = 33432;
+UPDATE gameobject_template SET flags = 6553632, data2 = 2000, ScriptName='go_red_button' WHERE entry = 194739;
+UPDATE creature_template SET ScriptName = 'mob_proximity_mine' WHERE entry = 34362;
+UPDATE creature_template SET ScriptName = 'mob_bomb_bot' WHERE entry IN (33836, 34192);
+UPDATE creature_template SET `faction_A` = 14, `faction_H` = 14, `minlevel` = 80, `maxlevel` = 80, ScriptName = 'mob_emergency_bot' WHERE entry = 34147;
+UPDATE creature_template SET ScriptName = 'mob_frost_bomb_ulduar' WHERE entry = 34149;
+UPDATE creature_template SET ScriptName = 'mob_mimiron_inferno' WHERE entry = 33370;
+UPDATE creature_template SET ScriptName = 'mob_assault_bot' WHERE entry = 34057;
+UPDATE creature_template SET ScriptName = 'mob_magnetic_core' WHERE entry = 34068;
+UPDATE `gameobject` SET `position_x` = 2734.73 WHERE `id` IN (194789, 194956);
+-- spells, may not be correct
+REPLACE INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES ('64444', '1', '33670');
+REPLACE INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES ('63414', '1', '33651');
+REPLACE INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES ('65101', '1', '33350');
+-- REPLACE INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES ('62909', '1', '33350');
+
+-- SOME MIMIRON LOOT BOXES ARE MISSING IN YTDB, THIS IS THE FIX FOR IT!
+-- mimiron loot fix:
+-- INSERT two new boxes
+DELETE FROM `gameobject` WHERE `id` IN (194957, 194958);
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+(110004, 194957, 603, 1, 65535, 2734.73, 2568.98, 364.314, 0.0139475, 0, 0, 0.00697369, 0.999976, -604800, 100, 1);
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+(110005, 194958, 603, 2, 65535, 2734.73, 2568.98, 364.314, 0.0139475, 0, 0, 0.00697369, 0.999976, -604800, 100, 1);
+-- 10 man hard:
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=194957);
+INSERT INTO `gameobject_loot_template` VALUES 
+(194957, 45089, 5, 0, -45089, 1, 0, 0, 0),
+(194957, 45095, 2.0408, 0, 1, 1, 0, 0, 0),
+(194957, 45649, 100, 0, -45649, 1, 0, 0, 0),
+(194957, 45663, 0.4028, 0, 1, 1, 0, 0, 0),
+(194957, 45787, -100, 0, 1, 1, 0, 0, 0),
+(194957, 47241, 100, 0, 1, 1, 0, 0, 0),
+(194957, 45972, 0, 1, 1, 1, 0, 0, 0),
+(194957, 45973, 0, 1, 1, 1, 0, 0, 0),
+(194957, 45974, 0, 1, 1, 1, 0, 0, 0),
+(194957, 45975, 0, 1, 1, 1, 0, 0, 0),
+(194957, 45976, 0, 1, 1, 1, 0, 0, 0),
+(194957, 45982, 0, 2, 1, 1, 0, 0, 0),
+(194957, 45988, 0, 2, 1, 1, 0, 0, 0),
+(194957, 45989, 0, 2, 1, 1, 0, 0, 0),
+(194957, 45990, 0, 2, 1, 1, 0, 0, 0),
+(194957, 45993, 0, 2, 1, 1, 0, 0, 0);
+-- 25 man hard:
+DELETE FROM `gameobject_loot_template` WHERE (`entry`=194958);
+INSERT INTO `gameobject_loot_template` VALUES 
+(194958, 45038, 10, 0, 1, 1, 0, 0, 0),
+(194958, 45087, 33, 0, 1, 1, 0, 0, 0),
+(194958, 45089, 5, 0, -45089, 1, 0, 0, 0),
+(194958, 45643, 100, 0, -45643, 1, 0, 0, 0),
+(194958, 45816, -100, 0, 1, 1, 0, 0, 0),
+(194958, 47241, 100, 0, 1, 1, 0, 0, 0),
+(194958, 45489, 0, 1, 1, 1, 0, 0, 0),
+(194958, 45490, 0, 1, 1, 1, 0, 0, 0),
+(194958, 45491, 0, 1, 1, 1, 0, 0, 0),
+(194958, 45492, 0, 1, 1, 1, 0, 0, 0),
+(194958, 45493, 0, 1, 1, 1, 0, 0, 0),
+(194958, 45494, 0, 2, 1, 1, 0, 0, 0),
+(194958, 45495, 0, 2, 1, 1, 0, 0, 0),
+(194958, 45496, 0, 2, 1, 1, 0, 0, 0),
+(194958, 45497, 0, 2, 1, 1, 0, 0, 0),
+(194958, 45663, 0, 2, 1, 1, 0, 0, 0);
